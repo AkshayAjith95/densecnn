2e9bd218ce3b97dc56f3e8c00bde5e28645e8bee
AkshayAjith95 <139571694+AkshayAjith95@users.noreply.github.com>
2023-07-23 19:29:48 +0530

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,85 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Set the seed for reproducibility
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# Data directories
+train_data_dir = "C:/Users/aksha/OneDrive/Documents/glaucoma detection dataset/training"
+validation_data_dir= "C:/Users/aksha/OneDrive/Documents/glaucoma detection dataset/validation"
+test_data_dir = "C:/Users/aksha/OneDrive/Documents/glaucoma detection dataset/test"
+
+
+# Image dimensions
+img_width, img_height = 150, 150
+
+# Batch size and number of epochs
+batch_size = 32
+epochs = 20
+
+# Data Augmentation and Preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+# Create the CNN model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0001), metrics=['accuracy'])
+
+# Model training
+model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // batch_size
+)
+
+# Model evaluation
+loss, accuracy = model.evaluate(test_generator)
+print(f'Test loss: {loss}')
+print(f'Test accuracy: {accuracy}')
